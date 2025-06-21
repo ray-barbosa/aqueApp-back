5e1e98b406a579e6ccddca89117f84d347c649ed
Ray <rayanebarbosa.dev@gmail.com>
2025-06-21 04:56:11 +0000

    feat(user): adiciona modelo User com validação e endpoint de registro

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db';
+import authRoutes from "./routes/authRoutes";
+
 
 
 dotenv.config();
@@ -14,6 +16,8 @@ app.use(express.json());
 app.get('/', (req, resp) => {
     resp.send('AquéApp Backend is running!')
 });
+
+app.use("/api", authRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
---
+++ src/routes/authRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { registerUser } from "../controllers/authController";
+
+const router = Router();
+
+router.post("/register", registerUser);
+
+export default router;
+
